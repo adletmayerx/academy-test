7af3c12180dd2bfff299732dd0268959de8bcaca
adletmayerx <adletmayerx@users.noreply.github.com>
2023-05-03 12:38:05 +0500

    fix: usRandomColor

--- frontend/fragments/landing-state-action/src/hooks/useRandomColor.ts
+++ frontend/fragments/landing-state-action/src/hooks/useRandomColor.ts
@@ -1,19 +1,16 @@
-import { useEffect } from 'react'
-import { useRef }    from 'react'
+import react, { MutableRefObject, RefObject, useEffect } from "react";
 
-const useRandomColor = () => {
-  const node = useRef(null)
-  const { random, floor } = Math
-  const colors = ['red', 'green', 'blue']
+const useRandomColor = (node: RefObject<Text>) => {
+  const { random, floor } = Math;
 
   useEffect(() => {
+    const colors = ["red", "green", "blue"];
     if (node && node.current) {
-      (node.current as any).style.color = colors[floor(random() * 3)]
+      (node.current as any).style.color = colors[floor(random() * 3)];
     }
-    
-  }, [])
+  }, [node, floor, random]);
 
-  return () => node
-}
+  return node;
+};
 
-export { useRandomColor }
+export { useRandomColor };
